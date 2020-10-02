c6b1dafa80c3b3b4ebcafd3f8cdce8ee8226ea15
VISakura <weloveanimeyuri@gmail.com>
2020-10-02 21:41:49 +0700

    HM
    Signed-off-by: VISakura <weloveanimeyuri@gmail.com>

--- dumpyara.sh
+++ dumpyara.sh
@@ -26,7 +26,7 @@ else
     [[ -e "$URL" ]] || { echo "Invalid Input" && exit 1; }
 fi
 
-ORG=ZyCromerZ #your GitHub org name
+ORG=VISakura #your GitHub org name
 FILE=$(echo ${URL##*/} | inline-detox)
 EXTENSION=$(echo ${URL##*.} | inline-detox)
 UNZIP_DIR=${FILE/.$EXTENSION/}
