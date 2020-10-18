f8859786cfcbd90001680113ff341a26dd495322
Hazukashio <hazukashio@gmail.com>
2020-10-18 19:10:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Link="https://dlcdnets.asus.com/pub/ASUS/ZenFone/ZB601KL/UL-ASUS_X00T-WW-17.2017.2008.432-user.zip"
-FileName="ASUS-X00T-WW-432"
+Link="https://dlcdnets.asus.com/pub/ASUS/ZenFone/ZS661KS/UL-ASUS_I003_1-ASUS-17.0823.2008.78-1.1.63-user.zip"
+FileName="ASUS-ROG-3"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
 wget "$Link" -O "$FileName"
