d26c1b57c16581045f6d6e3d2be7a2604ad26e04
vcyzteen <vcyzscape@gmail.com>
2021-07-12 00:38:21 +0700

    update
    Signed-off-by: vcyzteen <vcyzscape@gmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Link="https://otafsg1.h2os.com/patch/amazone2/GLO/NordOxygen/NordOxygen_14.O.11_GLO_011_2010232026/NordOxygen_14.O.11_OTA_011_all_2010232026_83bb980e97.zip"
-FileName="Nord"
+Link="https://rom.download.nubia.com/Europe&Asia/NX669J/V412/NX669J-update.zip"
+FileName="NR6"
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
 wget "$Link" -O "$FileName"
