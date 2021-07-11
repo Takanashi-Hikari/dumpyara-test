fd058e8a3be0243ff5888ccca4a3bf85046e644f
©TwT <vcyzscape@gmail.com>
2021-07-12 01:08:19 +0700

    update

--- dumpyara.sh
+++ dumpyara.sh
@@ -31,7 +31,7 @@ else
     [[ -e "$URL" ]] || { echo "Invalid Input" && exit 1; }
 fi
 
-ORG=Calliope-K #your GitHub org name
+ORG=vcyzteen #your GitHub org name
 FILE=$(echo ${URL##*/} | inline-detox)
 EXTENSION=$(echo ${URL##*.} | inline-detox)
 UNZIP_DIR=${FILE/.$EXTENSION/}
@@ -186,8 +186,8 @@ find "$PROJECT_DIR"/working/"${UNZIP_DIR}" -type f -printf '%P\n' | sort | grep 
 if [[ -n $GIT_OAUTH_TOKEN ]]; then
     curl --silent --fail "https://raw.githubusercontent.com/$ORG/$repo/$branch/all_files.txt" 2> /dev/null && echo "Firmware already dumped!" && exit 1
     git init
-    git config user.name Calliope-K
-    git config user.email firsname76@gmail.com
+    git config user.name vcyzteen
+    git config user.email vcyzscape@gmail.com
     git checkout -b "$branch"
     find . -size +97M -printf '%P\n' -o -name "*sensetime*" -printf '%P\n' -o -name "*.lic" -printf '%P\n' >| .gitignore
     git add --all
